94849aa28e9ff47058ba1b5d03f5beb6ca58b3f3
andrewj86q <quick.art2373@fastmail.com>
2025-12-03 10:46:29 +0000

    Fix the bug where the lang was not loaded when debug flag was passed #382

--- app/backend.server/translations.ts
+++ app/backend.server/translations.ts
@@ -16,45 +16,51 @@ type Translations = Record<string, TranslationEntry>;
 const loadedLangs: Record<string, Translations> = {
 };
 
+const DEBUG_SUFFIX = "-debug"
+
 // Cache loaded languages
-function loadLang(lang: string): Translations {
-  if (loadedLangs[lang]) return loadedLangs[lang];
-  try {
-    const filePath = join(__dirname, "..",  'locales', `${lang}.json`);
-    const content = readFileSync(filePath, 'utf-8');
-    loadedLangs[lang] = JSON.parse(content);
-    return loadedLangs[lang];
-  } catch {
-    loadedLangs[lang] = {};
-    return {};
-  }
+function loadLang(langWithDebug: string): Translations {
+	let lang = langWithDebug
+	if (lang.endsWith(DEBUG_SUFFIX)) {
+		lang = lang.slice(0, -DEBUG_SUFFIX.length);
+	}
+	if (loadedLangs[lang]) return loadedLangs[lang];
+	try {
+		const filePath = join(__dirname, "..", 'locales', `${lang}.json`);
+		const content = readFileSync(filePath, 'utf-8');
+		loadedLangs[lang] = JSON.parse(content);
+		return loadedLangs[lang];
+	} catch {
+		loadedLangs[lang] = {};
+		return {};
+	}
 }
 
 // Load flat record of translations. Only defaultMessage (the actual string, not other fields, such as descriptions)
 export function loadTranslations(lang: string): Record<string, string> {
-  const raw: Translations = loadLang(lang);
-  const flat: Record<string, string> = {};
-  for (const key in raw) {
-    const entry = raw[key];
-    if (entry?.defaultMessage !== undefined) {
-      flat[key] = entry.defaultMessage;
-    }
-  }
-  return flat;
+	const raw: Translations = loadLang(lang);
+	const flat: Record<string, string> = {};
+	for (const key in raw) {
+		const entry = raw[key];
+		if (entry?.defaultMessage !== undefined) {
+			flat[key] = entry.defaultMessage;
+		}
+	}
+	return flat;
 }
 
 const defaultLang = "en";
 
 // Create translator for lang; fallback to en or passed message
 export function createTranslationGetter(lang: string) {
-  return (p: TParams): string => {
-    const { code, msg } = p;
-    const data = loadLang(lang);
-		if (data[code]?.defaultMessage !== undefined){
+	return (p: TParams): string => {
+		const { code, msg } = p;
+		const data = loadLang(lang);
+		if (data[code]?.defaultMessage !== undefined) {
 			return data[code].defaultMessage
 		}
 		const dataDefautlLang = loadLang(defaultLang)
 		return dataDefautlLang[code]?.defaultMessage ?? msg
-  };
+	};
 };
 

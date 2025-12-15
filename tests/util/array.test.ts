19b5ba59b2ce037e32422759fd173fb2956ccb26
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-12-15 15:01:53 +0700

    #393 added vitest.config.ts and one test file for util/array.ts

---
+++ tests/util/array.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+import { eqArr } from '~/util/array';
+
+describe('eqArr', () => {
+    it('Should return true if both arrays are equal', () => {
+        const arr1 = [1, 2, 3];
+        const arr2 = [1, 2, 3];
+        expect(eqArr(arr1, arr2)).toBe(true);
+    });
+    it('Should return false if both arrays are not equal', () => {
+        const arr1 = [1, 2, 4];
+        const arr2 = [1, 2, 3];
+        expect(eqArr(arr1, arr2)).toBe(false);
+    });
+    it('Should return false if both arrays are not equal in types', () => {
+        const arr1 = [1, 2, '3'];
+        const arr2 = [1, 2, 3];
+        expect(eqArr(arr1, arr2)).toBe(false);
+    });
+    it('Should return false if size of both arrays are not equal', () => {
+        const arr1 = [1, 2];
+        const arr2 = [1, 2, 3];
+        expect(eqArr(arr1, arr2)).toBe(false);
+    });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,34 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+import { mergeConfig } from 'vite';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            environment: 'node',
+            globals: true,
+
+            coverage: {
+                provider: 'v8',
+
+                // 🔹 This replaces `all: true` in v4
+                include: ['app/**/*.{ts,tsx}'],
+
+                exclude: [
+                    '**/*.test.{ts,tsx}',
+                    '**/*.spec.{ts,tsx}',
+                    '**/__tests__/**',
+                    '**/node_modules/**',
+                    '**/build/**',
+                    '**/dist/**',
+                    'app/entry.client.tsx',
+                    'app/entry.server.tsx',
+                ],
+
+                reporter: ['text', 'html', 'lcov'],
+            },
+        },
+    }),
+);

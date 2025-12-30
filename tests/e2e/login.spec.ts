402e8b5feaa6cb95e9a89dc591e71b56e2c4f040
Haroon Nasher <haroon_foad2003@yahoo.com>
2025-12-30 13:27:31 +0700

    #393 added e2e test case when invalid credential using playwright

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login page', () => {
-    test('loads user login page', async ({ page }) => {
+    test('should loads user login page when navigating to /login', async ({ page }) => {
         await page.goto('/login');
 
         await expect(page).toHaveTitle(/Sign-in/i);
@@ -10,5 +10,15 @@ test.describe('User login page', () => {
         await expect(page.locator('input[name="email"]')).toBeVisible();
         await expect(page.locator('input[name="password"]')).toBeVisible();
         await expect(page.locator('#login-button')).toBeVisible();
+    });
+    test('should shows error on invalid credentials', async ({ page }) => {
+        await page.goto('/login');
+
+        await page.fill('input[name="email"]', 'wrong@test.com');
+        await page.fill('input[name="password"]', 'wrongpassword');
+
+        await page.click('#login-button');
+
+        await expect(page.getByText(/email or password do not match/i)).toBeVisible();
     });
 });

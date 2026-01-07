f7dfe926b33d82c2b5ca6fdd9b67c06c6794431b
Haroon Nasher <haroon_foad2003@yahoo.com>
2026-01-07 18:46:42 +0700

    #393 added e2e test case for listing hazardous event

---
+++ tests/e2e/hazardous-event/list-hazardous-event.spec.ts
@@ -0,0 +1,103 @@
+import { test, expect } from '@playwright/test';
+import {
+    countryAccounts,
+    eventTable,
+    hazardousEventTable,
+    instanceSystemSettings,
+    userCountryAccounts,
+    userTable,
+} from '~/drizzle/schema';
+import { dr, initDB } from '~/db.server';
+import bcrypt from 'bcryptjs';
+import { eq } from 'drizzle-orm';
+
+const testEmail = `e2e_${Date.now()}@test.com`;
+const userId = 'c6c1f0b9-2a6d-4f1a-9f6c-8d4e2a7b91f4';
+const countryAccountId = 'a9b4a2d1-6f98-4c3e-8b72-1a9f5d0c6e85';
+const eventId1 = 'b7b4a2d1-6f98-4c3e-8b72-1a9f5d0c6e11';
+const hazardousEventId1 = eventId1;
+const eventId2 = 'b8b4a2d1-6f98-4c3e-8b72-1a9f5d0c6e11';
+const hazardousEventId2 = eventId2;
+
+test.beforeAll(async () => {
+    initDB();
+
+    const passwordHash = bcrypt.hashSync('Password123!', 10);
+
+    await dr.transaction(async (tx) => {
+        await tx.insert(userTable).values({
+            id: userId,
+            email: testEmail,
+            password: passwordHash,
+            emailVerified: true,
+        });
+
+        await tx.insert(countryAccounts).values({
+            id: countryAccountId,
+            shortDescription: 'description',
+            countryId: 'e34ef71f-0a72-40c4-a6e0-dd19fb26f391',
+            status: 1,
+            type: 'Training',
+        });
+
+        await tx.insert(userCountryAccounts).values({
+            userId: userId,
+            countryAccountsId: countryAccountId,
+            role: 'admin',
+            isPrimaryAdmin: true,
+        });
+
+        await tx.insert(instanceSystemSettings).values({
+            countryAccountsId: countryAccountId,
+            approvedRecordsArePublic: true,
+        });
+        await tx.insert(eventTable).values({ id: eventId1 });
+        await tx.insert(hazardousEventTable).values({
+            id: hazardousEventId1,
+            hipTypeId: '1037',
+            countryAccountsId: countryAccountId,
+            approvalStatus: 'draft',
+            startDate: '2026-01-06',
+            endDate: '2026-01-07',
+            recordOriginator: '1',
+        });
+        await tx.insert(eventTable).values({ id: eventId2 });
+        await tx.insert(hazardousEventTable).values({
+            id: hazardousEventId2,
+            hipTypeId: '1037',
+            countryAccountsId: countryAccountId,
+            approvalStatus: 'draft',
+            startDate: '2026-01-06',
+            endDate: '2026-01-07',
+            recordOriginator: '1',
+        });
+    });
+});
+test.afterAll(async () => {
+    await dr.delete(hazardousEventTable).where(eq(hazardousEventTable.id, hazardousEventId1));
+    await dr.delete(eventTable).where(eq(eventTable.id, eventId1));
+    await dr.delete(hazardousEventTable).where(eq(hazardousEventTable.id, hazardousEventId2));
+    await dr.delete(eventTable).where(eq(eventTable.id, eventId2));
+    await dr
+        .delete(instanceSystemSettings)
+        .where(eq(instanceSystemSettings.countryAccountsId, countryAccountId));
+    await dr
+        .delete(userCountryAccounts)
+        .where(eq(userCountryAccounts.countryAccountsId, countryAccountId));
+    await dr.delete(countryAccounts).where(eq(countryAccounts.id, countryAccountId));
+    await dr.delete(userTable).where(eq(userTable.id, userId));
+});
+
+test.describe('List Hazardous event page', () => {
+    test('should successfully show two hazardous event in the table when there are only two records of hazardous event', async ({
+        page,
+    }) => {
+        await page.goto('/en/user/login');
+
+        await page.fill('input[name="email"]', testEmail);
+        await page.fill('input[name="password"]', 'Password123!');
+        page.click('#login-button');
+
+        await expect(page.getByTestId('list-table').locator('tbody tr')).toHaveCount(2);
+    });
+});

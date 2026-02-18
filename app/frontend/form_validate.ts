2f74de4a9f093c0dfe6d847095f9514f7cd67d2b
andrewj86q <quick.art2373@fastmail.com>
2026-02-18 09:38:13 +0000

    BUG: in api requests allow pasing null for money and enum-flex fields #433

--- app/frontend/form_validate.ts
+++ app/frontend/form_validate.ts
@@ -294,12 +294,16 @@ export function validateFromJson<T>(
                 return value;
             case 'text':
             case 'textarea':
+                if (typeof value != 'string' && value !== undefined && value !== null) {
+                    throw invalidTypeError(field, 'string', value);
+                }
+                return value || '';
             case 'money':
             case 'enum-flex':
                 if (typeof value != 'string' && value !== undefined && value !== null) {
                     throw invalidTypeError(field, 'string', value);
                 }
-                return value || '';
+                return value || null;
             case 'date':
             case 'datetime':
                 if (value !== undefined && value !== null) {

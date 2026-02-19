4b627adbe043183d4237f7b575677107cfb1fd03
Haroon Nasher <haroon_foad2003@yahoo.com>
2026-02-19 12:20:57 +0700

    fix upgrade_database.sql to be sequential upgrade chain

--- scripts/dts_database/upgrade_database.sql
+++ scripts/dts_database/upgrade_database.sql
@@ -1,33 +1,29 @@
--- ================================================
--- DELTA DATABASE UPGRADE SCRIPT
--- USE THIS FILE ONLY WHEN YOU WANT TO UPGRADE THE 
--- DATABASE FROM PREVIOUS VERSIONS TO THE CURRENT VERSION.
--- DO NOT USE THIS FILE FOR FRESH DATABASE INSTALLATION.
--- ================================================
 BEGIN;
 \encoding UTF8
 SHOW client_encoding;
 
-
 \echo Checking current database version...
 
-    SELECT
-        (version_no = '0.1.1') AS is_version_0_1_1,
-        (version_no = '0.1.2') AS is_version_0_1_2,
-        (version_no = '0.1.3') AS is_version_0_1_3
-    FROM dts_system_info
-    LIMIT 1
-    \gset
+-- ================================
+-- Upgrade 0.1.1 -> 0.1.3
+-- Note: the app version was named here 0.1.2 but the version in db was saved as 0.1.1
+-- ================================
+SELECT (version_no='0.1.1') AS is_version_0_1_1 FROM dts_system_info LIMIT 1 \gset
+\if :is_version_0_1_1
+    \echo Upgrading from 0.1.1 to 0.1.3...
+    \ir upgrade_from_0.1.2_to_0.1.3.sql
+    SELECT version_no FROM dts_system_info LIMIT 1 \gset
+\endif
 
-    \if :is_version_0_1_1
-        \echo Upgrading from 0.1.1 to 0.1.3...
-        \ir upgrade_from_0.1.2_to_0.1.3.sql
-    \elif :is_version_0_1_2
-        \echo Upgrading from 0.1.2 to 0.1.3...
-        \ir upgrade_from_0.1.2_to_0.1.3.sql
-    \elif :is_version_0_1_3
-        \echo Upgrading from 0.1.3 to 0.2.0...
-        \ir upgrade_from_0.1.3_to_0.2.0.sql
-    \endif
+-- ================================
+-- Upgrade 0.1.3 -> 0.2.0
+-- ================================
+SELECT (version_no='0.1.3') AS is_version_0_1_3 FROM dts_system_info LIMIT 1 \gset
+\if :is_version_0_1_3
+    \echo Upgrading from 0.1.3 to 0.2.0...
+    \ir upgrade_from_0.1.3_to_0.2.0.sql
+    SELECT version_no FROM dts_system_info LIMIT 1 \gset
+\endif
+
 
 COMMIT;
